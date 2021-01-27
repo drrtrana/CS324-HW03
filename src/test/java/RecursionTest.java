9226a07e737cc1e56e2e3b2583cce6122d85391f
Rachel Trana <retrana@neiu.edu>
2021-01-26 21:22:07 -0600

    Initial commit

---
+++ src/test/java/RecursionTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class RecursionTest {
+
+    /*@Test
+    void hopSteps_shouldTestOneStep() {
+        assertEquals(1, Recursion.hopSteps(1));
+    }*/
+
+    /*@Test
+    void hopSteps_shouldTestTwoSteps() {
+        assertEquals(2, Recursion.hopSteps(2));
+    }*/
+
+    /*@Test
+    void hopSteps_shouldTestThreeSteps() {
+        assertEquals(4, Recursion.hopSteps(3));
+    }*/
+
+    /*@Test
+    void hopSteps_shouldTestFourSteps() {
+        assertEquals(7, Recursion.hopSteps(4));
+    }*/
+
+    /*@Test
+    void hopSteps_shouldTestTwelveSteps() {
+        assertEquals(927, Recursion.hopSteps(12));
+    }*/
+}
